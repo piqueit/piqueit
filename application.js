115fd3622bfb0f25b94af166832de7671ed7b2d3
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2015-11-07 16:27:30 -0800

    add csv file

--- application.js
+++ application.js
@@ -1,1 +1,13 @@
-<
+$(document).ready(){
+  $('a').click(updatePage)
+}
+
+var updatePage = function(e){
+  e.preventDefault()
+  conosle.log("hit")
+  url = $(this).attr('href')
+
+  $.get(url, function(data){
+    $('body').html(data)
+  })
+}
