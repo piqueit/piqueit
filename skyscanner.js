2fda4874b33511e46dcbb538160ddc4b98dbd413
Dian Pan <dian.di.pan@gmail.com>
2015-11-07 23:47:33 -0800

    ajax call

--- skyscanner.js
+++ skyscanner.js
@@ -1,13 +1,25 @@
 var skyscanner = function(user){
+  var today = new Date();
   var data = {
     apiKey: "ah328032345045357940601162731181",
     country: user.country,
-
+    currency: "USD",
+    locale:"en-US",
+    originplace: user.city,
+    destinationplace: "DBX",
+    outbounddate: "2015-11-" + (today.getDate() + 7),
+    inbounddate: "2015-11-" + (today.getDate() + 14)
   }
-  console.log(data);
   $.ajax({
-    url: "http://partners.api.skyscanner.net/apiservices/pricing/v1.0"
+    url: "http://partners.api.skyscanner.net/apiservices/pricing/v1.0",
+    method: "post",
+    data: data,
+    dataType: "json"
   })
+  .done( function(){
+    console.log("we made it")
+  })
+
 }
 
 
